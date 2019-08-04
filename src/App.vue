d4a7ad0060d09a9948a8e2c45207567c468d805f
Joe T. Wilson <jtwilson.home@gmail.com>
2019-08-04 15:07:38 -0600

    fixed icon size, made title larger

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
-    <div id="app">
-        <cookie-consent class="justify-self-center"/>
+  <div id="app">
+    <cookie-consent class="justify-self-center" />
 
-        <div class="card mx-auto w-50 mt-2 py-1 bg-secondary border-dark text-light">
-            <h1 class="text-center">MTG Binder Finder</h1>
-        </div>
+    <h1 class="text-white text-center display-1 my-5">MTG Binder Finder</h1>
 
-        <Submission @cards-submitted="onSubmit" v-model="cardListText"></Submission>
+    <Submission @cards-submitted="onSubmit" v-model="cardListText"></Submission>
 
-        <div class="mx-auto w-75">
-            <button
-                @click="sortSets"
-                v-if="cardsReceived"
-                class="mb-2 btn btn-warning border-dark justify-self-right"
-            >Re-Sort</button>
-        </div>
-
-        <div id="accordion">
-            <div v-for="set in sortedSetNames" :key="set">
-                <set-view
-                    @rem-card="removeAllCards"
-                    @rem-printing="removePrinting"
-                    :set="sets[set]"
-                ></set-view>
-            </div>
-        </div>
+    <div class="mx-auto w-75">
+      <button
+        @click="sortSets"
+        v-if="cardsReceived"
+        class="mb-2 btn btn-warning border-dark justify-self-right"
+      >Re-Sort</button>
     </div>
+
+    <div id="accordion">
+      <div v-for="set in sortedSetNames" :key="set">
+        <set-view @rem-card="removeAllCards" @rem-printing="removePrinting" :set="sets[set]"></set-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -40,209 +34,198 @@ import bottom from "vue-cookieconsent-component/src/scss/_cookie-consent-bottom.
 import trans from "vue-cookieconsent-component/src/scss/_cookie-consent-transition.scss";
 
 export default {
-    name: "app",
-    components: {
-        Submission,
-        SetView
-    },
-    data() {
-        return {
-            cardListText: "",
-            cardName: "",
-            cardNames: "",
-            queryResult: "",
-            sets: {},
-            parsedCards: {},
-            queryText: "",
-            sortedSetNames: []
-        };
-    },
-    mounted: function() {
-        $('[data-toggle="popover"]').popover({
-            trigger: "focus",
-            html: true
-        });
-    },
-    computed: {
-        cardsReceived: function() {
-            return Object.keys(this.sets).length > 1;
-        }
-    },
-    methods: {
-        onSubmit() {
-            this.clearSets();
-            this.parseInput();
-            this.getData();
-        },
-        clearSets() {
-            Object.keys(this.sets).forEach(setCode => {
-                var set = this.sets[setCode];
-                if (set.cards) {
-                    set.cards = [];
-                }
-            });
-        },
-        parseInput() {
-            this.cardNames = this.cardListText;
-            if (isNaN(this.cardNames[0])) {
-                this.cardNames = "1 " + this.cardNames;
-                this.cardNames = this.cardNames.replace(
-                    /(?:\r\n|\r|\n)/g,
-                    "\n1 "
-                );
-            }
-            this.parsedCards = mtgparser(this.cardNames, "mtgo");
-            this.parsedCards["cardArrays"] = [];
-            this.splitCardArray(
-                this.parsedCards.cards,
-                this.parsedCards.cardArrays
-            );
-            //TODO
-        },
-        splitCardArray(array, dest) {
-            if (array.length > 45) {
-                dest.push(array.slice(0, 45));
-                this.splitCardArray(array.slice(45), dest);
-            } else {
-                dest.push(array);
-            }
-        },
-        getData() {
-            this.parsedCards.cardArrays.forEach((array, j) => {
-                this.queryText = 'https://api.scryfall.com/cards/search?q=!"';
-
-                array.forEach((el, i) => {
-                    if (
-                        !(
-                            el.name.toUpperCase() === "MOUNTAIN" ||
-                            el.name.toUpperCase() === "PLAINS" ||
-                            el.name.toUpperCase() === "ISLAND" ||
-                            el.name.toUpperCase() === "SWAMP" ||
-                            el.name.toUpperCase() === "FOREST"
-                        )
-                    ) {
-                        if (el.name.length > 0) {
-                            this.queryText += el.name.replace(/ /g, "+");
-                            if (
-                                j + 1 < this.parsedCards.cardArrays.length ||
-                                i + 1 < array.length
-                            ) {
-                                this.queryText += '"+OR+!"';
-                            }
-                        }
-                    }
-                });
-
-                this.queryText += '"&unique=prints&order=color';
-
-                axios
-                    .get(this.queryText)
-                    .then(res => {
-                        this.queryResult = res;
-                        this.populateSets();
-                        this.getHasMore();
-                    })
-                    .catch(err => {
-                        // handle error
-                        console.log(err);
-                    });
-            });
-        },
-        getHasMore() {
-            if (this.queryResult.data.has_more) {
-                axios.get(this.queryResult.data.next_page).then(res => {
-                    this.queryResult = res;
-                    this.populateSets();
-                    this.getHasMore();
-                    //this.sortSets();
-                });
-            } else {
-                this.sortSets();
-            }
-        },
-        populateSets() {
-            for (const card of this.queryResult.data.data) {
-                if (
-                    card.promo ||
-                    card.set_type == "planechase" ||
-                    card.set_type == "duel_deck" ||
-                    card.set_type == "box" ||
-                    card.set_type == "spellbook" ||
-                    card.set_type == "premium_deck" ||
-                    card.set == "sum" ||
-                    card.set == "4bb" ||
-                    card.set == "fbb" ||
-                    !card.games.includes("paper")
-                ) {
-                    continue;
-                }
-
-                if (card.set_type == "commander") {
-                    card.set = "c18";
-                    card.set_name = "Commander Sets";
-                }
-
-                if (!Object.keys(this.sets).includes(card.set)) {
-                    this.$set(this.sets, card.set, {
-                        setCode: card.set,
-                        setName: card.set_name,
-                        cards: [card],
-                        setUri: card.set_uri
-                    });
-                } else {
-                    if (
-                        !this.setContainsCardName(
-                            this.sets[card.set],
-                            card.name
-                        )
-                    ) {
-                        this.sets[card.set].cards.push(card);
-                    }
-                }
-            }
-        },
-        sortSets() {
-            this.sortedSetNames = Object.keys(this.sets);
-
-            this.sortedSetNames.sort((a, b) => {
-                var aLen = this.sets[a].cards.length;
-                var bLen = this.sets[b].cards.length;
-                //console.log("a: " + this.aLen + " b: " + this.bLen);
-                return bLen - aLen;
-            });
-            this.$forceUpdate;
-        },
-        setContainsCardName(set, cardName) {
-            for (let index = 0; index < set.cards.length; index++) {
-                if (set.cards[index].name === cardName) {
-                    return true;
-                }
-            }
-
-            return false;
-        },
-        removePrinting(setCode, card) {
-            var index = this.sets[setCode].cards.indexOf(card);
-            if (index != -1) {
-                this.sets[setCode].cards.splice(index, 1);
-            }
-        },
-        removeAllCards(cardName) {
-            Object.keys(this.sets).forEach(setCode => {
-                var set = this.sets[setCode];
-                for (var i = 0; i < set.cards.length; i++) {
-                    if (set.cards[i].name === cardName) {
-                        set.cards.splice(i, 1);
-                    }
-                }
-            });
-        }
+  name: "app",
+  components: {
+    Submission,
+    SetView
+  },
+  data() {
+    return {
+      cardListText: "",
+      cardName: "",
+      cardNames: "",
+      queryResult: "",
+      sets: {},
+      parsedCards: {},
+      queryText: "",
+      sortedSetNames: []
+    };
+  },
+  mounted: function() {
+    $('[data-toggle="popover"]').popover({
+      trigger: "focus",
+      html: true
+    });
+  },
+  computed: {
+    cardsReceived: function() {
+      return Object.keys(this.sets).length > 1;
     }
+  },
+  methods: {
+    onSubmit() {
+      this.clearSets();
+      this.parseInput();
+      this.getData();
+    },
+    clearSets() {
+      Object.keys(this.sets).forEach(setCode => {
+        var set = this.sets[setCode];
+        if (set.cards) {
+          set.cards = [];
+        }
+      });
+    },
+    parseInput() {
+      this.cardNames = this.cardListText;
+      if (isNaN(this.cardNames[0])) {
+        this.cardNames = "1 " + this.cardNames;
+        this.cardNames = this.cardNames.replace(/(?:\r\n|\r|\n)/g, "\n1 ");
+      }
+      this.parsedCards = mtgparser(this.cardNames, "mtgo");
+      this.parsedCards["cardArrays"] = [];
+      this.splitCardArray(this.parsedCards.cards, this.parsedCards.cardArrays);
+      //TODO
+    },
+    splitCardArray(array, dest) {
+      if (array.length > 45) {
+        dest.push(array.slice(0, 45));
+        this.splitCardArray(array.slice(45), dest);
+      } else {
+        dest.push(array);
+      }
+    },
+    getData() {
+      this.parsedCards.cardArrays.forEach((array, j) => {
+        this.queryText = 'https://api.scryfall.com/cards/search?q=!"';
+
+        array.forEach((el, i) => {
+          if (
+            !(
+              el.name.toUpperCase() === "MOUNTAIN" ||
+              el.name.toUpperCase() === "PLAINS" ||
+              el.name.toUpperCase() === "ISLAND" ||
+              el.name.toUpperCase() === "SWAMP" ||
+              el.name.toUpperCase() === "FOREST"
+            )
+          ) {
+            if (el.name.length > 0) {
+              this.queryText += el.name.replace(/ /g, "+");
+              if (
+                j + 1 < this.parsedCards.cardArrays.length ||
+                i + 1 < array.length
+              ) {
+                this.queryText += '"+OR+!"';
+              }
+            }
+          }
+        });
+
+        this.queryText += '"&unique=prints&order=color';
+
+        axios
+          .get(this.queryText)
+          .then(res => {
+            this.queryResult = res;
+            this.populateSets();
+            this.getHasMore();
+          })
+          .catch(err => {
+            // handle error
+            console.log(err);
+          });
+      });
+    },
+    getHasMore() {
+      if (this.queryResult.data.has_more) {
+        axios.get(this.queryResult.data.next_page).then(res => {
+          this.queryResult = res;
+          this.populateSets();
+          this.getHasMore();
+          //this.sortSets();
+        });
+      } else {
+        this.sortSets();
+      }
+    },
+    populateSets() {
+      for (const card of this.queryResult.data.data) {
+        if (
+          card.promo ||
+          card.set_type == "planechase" ||
+          card.set_type == "duel_deck" ||
+          card.set_type == "box" ||
+          card.set_type == "spellbook" ||
+          card.set_type == "premium_deck" ||
+          card.set == "sum" ||
+          card.set == "4bb" ||
+          card.set == "fbb" ||
+          !card.games.includes("paper")
+        ) {
+          continue;
+        }
+
+        if (card.set_type == "commander") {
+          card.set = "c18";
+          card.set_name = "Commander Sets";
+        }
+
+        if (!Object.keys(this.sets).includes(card.set)) {
+          this.$set(this.sets, card.set, {
+            setCode: card.set,
+            setName: card.set_name,
+            cards: [card],
+            setUri: card.set_uri
+          });
+        } else {
+          if (!this.setContainsCardName(this.sets[card.set], card.name)) {
+            this.sets[card.set].cards.push(card);
+          }
+        }
+      }
+    },
+    sortSets() {
+      this.sortedSetNames = Object.keys(this.sets);
+
+      this.sortedSetNames.sort((a, b) => {
+        var aLen = this.sets[a].cards.length;
+        var bLen = this.sets[b].cards.length;
+        //console.log("a: " + this.aLen + " b: " + this.bLen);
+        return bLen - aLen;
+      });
+      this.$forceUpdate;
+    },
+    setContainsCardName(set, cardName) {
+      for (let index = 0; index < set.cards.length; index++) {
+        if (set.cards[index].name === cardName) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+    removePrinting(setCode, card) {
+      var index = this.sets[setCode].cards.indexOf(card);
+      if (index != -1) {
+        this.sets[setCode].cards.splice(index, 1);
+      }
+    },
+    removeAllCards(cardName) {
+      Object.keys(this.sets).forEach(setCode => {
+        var set = this.sets[setCode];
+        for (var i = 0; i < set.cards.length; i++) {
+          if (set.cards[i].name === cardName) {
+            set.cards.splice(i, 1);
+          }
+        }
+      });
+    }
+  }
 };
 </script>
 
 <style>
 .popover {
-    max-width: 50%;
+  max-width: 50%;
 }
 </style>
--- src/components/SetView.vue
+++ src/components/SetView.vue
@@ -1,81 +1,81 @@
 <template>
-    <div v-show="hasLength" class="card mx-auto w-75 my-0 bg-secondary border-dark text-light">
-        <div
-            class="card-header py-0"
-            @mouseover="hover = true"
-            @mouseleave="hover = false"
-            data-toggle="collapse"
-            :data-target="'#'+set.setCode"
-            :class="{ active: hover }"
-        >
-            <div class="row">
-                <div class="col align-self-center py-3 pl-1">
-                    <span class="badge badge-warning">{{this.set.cards.length}}</span>
-                    {{set.setName}}
-                </div>
+  <div v-show="hasLength" class="card mx-auto w-75 my-0 bg-secondary border-dark text-light">
+    <div
+      class="card-header py-0"
+      @mouseover="hover = true"
+      @mouseleave="hover = false"
+      data-toggle="collapse"
+      :data-target="'#'+set.setCode"
+      :class="{ active: hover }"
+    >
+      <div class="row">
+        <div class="col align-self-center py-3 pl-1">
+          <span class="badge badge-warning">{{this.set.cards.length}}</span>
+          {{set.setName}}
+        </div>
 
-                <img
-                    class="col-2 col-sm-2 col-md-1 img-fluid p-3"
-                    :src="'https://img.scryfall.com/sets/'+this.set.setCode+'.svg'"
-                >
-            </div>
-        </div>
-        <div class="collapse" :id="set.setCode" data-parent="#accordion">
-            <div class="card-body">
-                <div class="row">
-                    <CardView
-                        @rem-card="removeCard"
-                        @rem-printing="removePrinting"
-                        v-for="card in set.cards"
-                        :key="card.id"
-                        :card="card"
-                    ></CardView>
-                </div>
-            </div>
-        </div>
+        <img
+          class="col-2 col-sm-2 col-md-1 img-fluid p-2"
+          :src="'https://img.scryfall.com/sets/'+this.set.setCode+'.svg'"
+        />
+      </div>
     </div>
+    <div class="collapse" :id="set.setCode" data-parent="#accordion">
+      <div class="card-body">
+        <div class="row">
+          <CardView
+            @rem-card="removeCard"
+            @rem-printing="removePrinting"
+            v-for="card in set.cards"
+            :key="card.id"
+            :card="card"
+          ></CardView>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import CardView from "./CardView.vue";
 
 export default {
-    components: {
-        CardView
-    },
-    data() {
-        return {
-            hover: false
-        };
-    },
-    props: {
-        set: {
-            setCode: String,
-            setName: String,
-            cards: [],
-            setUri: String
-        }
-    },
-
-    computed: {
-        hasLength: function() {
-            return this.set.cards.length > 0 ? true : false;
-        }
-    },
-    methods: {
-        removePrinting(card) {
-            this.$emit("rem-printing", this.set.setCode, card);
-        },
-        removeCard(cardName) {
-            this.$emit("rem-card", cardName);
-        }
+  components: {
+    CardView
+  },
+  data() {
+    return {
+      hover: false
+    };
+  },
+  props: {
+    set: {
+      setCode: String,
+      setName: String,
+      cards: [],
+      setUri: String
     }
+  },
+
+  computed: {
+    hasLength: function() {
+      return this.set.cards.length > 0 ? true : false;
+    }
+  },
+  methods: {
+    removePrinting(card) {
+      this.$emit("rem-printing", this.set.setCode, card);
+    },
+    removeCard(cardName) {
+      this.$emit("rem-card", cardName);
+    }
+  }
 };
 </script>
 
 <style scoped>
 .active {
-    background-color: rgb(138, 144, 150);
+  background-color: rgb(138, 144, 150);
 }
 </style>
 
